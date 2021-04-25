5e62300fa8ce7262903ac84620d2101181d6e160
haileykim2014 <74589038+haileykim2014@users.noreply.github.com>
2021-04-25 14:27:53 +0900

    격자판 최대합
    -중첩for문이용
    -최대값 넣을 변수에는 최소값으로 초기화
    -행의 합 : 큰for문이 행, 작은for문이 열 i가 일때 j가 0~4
    -열의 합: [j][i]로 해서 , i가 0일때 j가 0~4로 열의 합계
    - Math.answer(answer,sum1) 최대값구하기
    -대각선의 합 :  대각선이 두개가있다. 10시방향 [i][i] , 2시방향[i][n-1-i]

--- Test.java
+++ Test.java
@@ -1,10 +1,46 @@
-package String;
+package testPackage;
+
+import java.util.Scanner;
 
 public class Test {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static int solution(int n,int[][] arr) {
+		int answer = Integer.MIN_VALUE;// 최대값대입해야하므로 최소값으로 초기화
+		int sum1,sum2;//행의 합,열의합
+		for(int i=0;i<n;i++) {
+			sum1=sum2=0;
+			for(int j=0;j<n;j++) {
+				sum1 += arr[i][j];//각 행의 합
+				sum2 += arr[j][i];//각 열의 합
+			}
+			answer = Math.max(answer, sum1);//행의 최대값
+			answer = Math.max(answer, sum2);//열합의 최대값			
+		}		
+		sum1 =sum2 =0;
+		for(int i=0;i<n;i++) {
+			sum1 +=arr[i][i];//대각선의합
+			sum2 +=arr[i][n-i-1];			
+		}
+		answer = Math.max(answer, sum1);
+		answer = Math.max(answer, sum2);
+		return answer;
 	}
 
+	public static void main(String[] args) {
+		Test t = new Test();
+		Scanner scan = new Scanner(System.in);
+		int n=scan.nextInt();
+		int a = 0;
+		int[][] arr=new int[n][n];
+		int[]answer=new int[n];
+		for(int i=0; i<n; i++){
+			for(int j=0; j<n; j++){
+				arr[i][j]=scan.nextInt();
+			}
+		}
+			System.out.println(t.solution(n, arr));
+
+	}
 }
+
+
+
