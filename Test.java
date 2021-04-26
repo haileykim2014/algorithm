8d4d6955fd9393bd11bebb1b54f5fdd07f8f6372
haileykim2014 <74589038+haileykim2014@users.noreply.github.com>
2021-04-27 01:14:14 +0900

    임시반장 정하기
    -i,j는 학생번호(i가 1일때 j가 1~5) 
    -n:명수
    -k는 학년
    -cnt 같은반이었던횟수
    -i번학생의 k학년과 j번학생의 k학년비교 (같은학년이엇을때)

--- Test.java
+++ Test.java
@@ -3,44 +3,37 @@ package testPackage;
 import java.util.Scanner;
 
 public class Test {
-	public static int solution(int n,int[][] arr) {
-		int answer = Integer.MIN_VALUE;// 최대값대입해야하므로 최소값으로 초기화
-		int sum1,sum2;//행의 합,열의합
-		for(int i=0;i<n;i++) {
-			sum1=sum2=0;
-			for(int j=0;j<n;j++) {
-				sum1 += arr[i][j];//각 행의 합
-				sum2 += arr[j][i];//각 열의 합
+	public int solution(int n, int[][] arr){
+		int answer=0, max=0;
+		for(int i=1; i<=n; i++){
+			int cnt=0;
+			for(int j=1; j<=n; j++){
+				for(int k=1; k<=5; k++){
+					if(arr[i][k]==arr[j][k]){
+						cnt++;
+						break;
+					}
+				}
 			}
-			answer = Math.max(answer, sum1);//행의 최대값
-			answer = Math.max(answer, sum2);//열합의 최대값			
-		}		
-		sum1 =sum2 =0;
-		for(int i=0;i<n;i++) {
-			sum1 +=arr[i][i];//대각선의합
-			sum2 +=arr[i][n-i-1];			
+			if(cnt>max){
+				max=cnt;
+				answer=i;
+			}
 		}
-		answer = Math.max(answer, sum1);
-		answer = Math.max(answer, sum2);
 		return answer;
 	}
 
-	public static void main(String[] args) {
-		Test t = new Test();
-		Scanner scan = new Scanner(System.in);
-		int n=scan.nextInt();
-		int a = 0;
-		int[][] arr=new int[n][n];
-		int[]answer=new int[n];
-		for(int i=0; i<n; i++){
-			for(int j=0; j<n; j++){
-				arr[i][j]=scan.nextInt();
+	public static void main(String[] args){
+		Test T = new Test();
+		Scanner kb = new Scanner(System.in);
+		int n=kb.nextInt();
+		int[][] arr=new int[n+1][6];
+		for(int i=1; i<=n; i++){
+			for(int j=1; j<=5; j++){
+				arr[i][j]=kb.nextInt();
 			}
 		}
-			System.out.println(t.solution(n, arr));
-
+		System.out.print(T.solution(n, arr));
 	}
 }
-
-
 
