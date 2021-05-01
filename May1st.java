b7ba99c48a4ae73a92bb6a363c4199f1665414bd
haileykim2014 <74589038+haileykim2014@users.noreply.github.com>
2021-05-02 00:12:31 +0900

    공통원소 구하기
    1.두배열을 먼저 오름차순으로 정렬시킨다.
    2.인덱스가 배열길이보다 작을동안 반복시킨다. while문안에 두 배열의 인덱스를 매개변수로 넣는다.
    3.첫번째배열과 두번째배열의 값이 같다면 두 배열의 인덱스를 증가시키고 그값을 add한다
    4.arr1보다 arr2가 크면 arr1의 인덱스를 증가시킨다.
    5.arr2가 작다면 arr2의 인덱스를 증가시킨다.

---
+++ May1st.java
@@ -0,0 +1,42 @@
+package testPackage;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class May1st {
+	public ArrayList<Integer> solution(int a, int b, int[] arr1, int[] arr2) {
+		ArrayList<Integer> answer = new ArrayList<>();
+		Arrays.sort(arr1);//오름차순정렬
+		Arrays.sort(arr2);
+		int p1=0, p2=0;
+		while(p1<a && p2<b){
+			if(arr1[p1]==arr2[p2]){
+				answer.add(arr1[p1++]);//같을때 동시증가
+				p2++;
+			}
+			else if(arr1[p1]<arr2[p2]) p1++; //작은배열먼저증가
+			else p2++;
+		}
+		return answer;
+	}
+	public static void main(String[] args){
+		May1st T = new May1st();
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int[] arr1 = new int[n];
+		for(int i
+				=0;i<n;i++)
+			arr1[i]= scan.nextInt();
+		
+		int m = scan.nextInt();
+		int arr2[] = new int[m];
+		for(int j=0;j<m;j++)
+			arr2[j]=scan.nextInt();		
+		
+		System.out.println(T.solution(n, m, arr1, arr2));
+	}
+
+
+}
