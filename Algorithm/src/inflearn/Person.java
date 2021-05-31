3b74867ed0ebf6fbee6bd238149078eb56d1c50e
haileykim2014 <haileykim2014@gmail.com>
2021-06-01 01:56:43 +0900

    응급실

---
+++ Algorithm/src/inflearn/Person.java
@@ -0,0 +1,53 @@
+package inflearn;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Person {
+	int id;
+	int priority;
+	public Person(int id, int priority) {
+		this.id = id;
+		this.priority = priority;
+	}
+}
+
+class June1{
+	public int solution (int n, int m,int[] arr) {
+		int answer = 0;
+		Queue<Person> Q = new LinkedList<>();
+		for(int i=0;i<n;i++) {
+			Q.offer(new Person(i,arr[i]));//i번쨰 객체를만들면서 q에 넣는다.
+		}
+		while(!Q.isEmpty()) {
+			Person tmp = Q.poll();
+			for(Person x:Q) {
+				if(x.priority>tmp.priority) {
+					Q.offer(tmp);
+					tmp = null;
+					break;
+				}
+			}
+			if(tmp!=null) {
+				answer++;
+				if(tmp.id==m)
+					return answer;
+			}
+		}
+		return answer;
+	}
+
+	
+	
+	public static void main(String[] args) {
+		June1 t = new June1();
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int m = scan.nextInt();
+		int[] arr = new int[n];
+		System.out.println(t.solution(n,m,arr));
+	}
+
+
+}
