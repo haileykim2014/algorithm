b4bed7716bb60cf4fc24351b502dd1519775bcca
haileykim2014 <haileykim2014@gmail.com>
2021-05-31 23:54:53 +0900

    교육과정설계

--- Algorithm/src/inflearn/May31.java
+++ Algorithm/src/inflearn/May31.java
@@ -4,27 +4,27 @@ import java.util.Queue;
 import java.util.Scanner;
 import java.util.Stack;
 
-public class May27 {
+public class May31 {//필수과목순서,
 	private String solution(String n,String k) {
 		String answer = "YES";
 		Queue<Character> q = new LinkedList<>();//큐선언, 타입은 인티저
 		
 		for(char x : n.toCharArray())
-			q.offer(x);
-		for(char x: k.toCharArray()) {
-			if(q.contains(x)) {
+			q.offer(x);//필수과목순서대로넣기
+		for(char x: k.toCharArray()) {//수업계획서
+			if(q.contains(x)) {// q안에있는지
 				if(x!=q.poll())
 					return "no";
 			}
 		}
-		if(!q.isEmpty())
+		if(!q.isEmpty())//뭔가 남아있을때
 			return "no";
 		return answer;
 	}
 	
 	
 	public static void main(String[] args) {
-		May27 t = new May27();
+		May31 t = new May31();
 		Scanner scan = new Scanner(System.in);
 		String n = scan.next();
 		String k = scan.next();
