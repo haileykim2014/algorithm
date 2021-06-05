2d993dac5eb96baf6a17da68dcf9aa41c2efacd8
haileykim2014 <haileykim2014@gmail.com>
2021-06-05 13:18:20 +0900

    LRU(캐시, 카카오 변형)

--- Algorithm/src/inflearn/June5th.java
+++ Algorithm/src/inflearn/June5th.java
@@ -13,16 +13,15 @@ public class June5th {
 				if(x==cache[i])//캐시에 같은 값이있는지?
 					pos =i;
 			if(pos==-1) {//miss라면, 해야할 작업이 캐시에없는상태
-				for(int i=size-1;i>=1;i--) {//히트난지점부터 for문돌리기
-					cache[i]=cache[i-1];
+				for(int i=size-1;i>=1;i--) {//캐시사이즈만큼 반복하고
+					cache[i]=cache[i-1];//뒤로밀기
 				}
 				cache[0] =x;//arr의 현재x값이 0번쨰인덱스에온다
-			}
-			else {//히트일때, 해야할 작업이 캐시에없을때
-				for(int i=pos;i>=1;i--) {
+			}else {//히트일때, 해야할 작업이 캐시에 있을때
+				for(int i=pos;i>=1;i--) {//히트난 지점부터 1까지 반복
 					cache[i]=cache[i-1];
 				}
-				cache[0]=x;
+				cache[0]=x;//현재작업넣기
 			}
 		}
 		return cache;
