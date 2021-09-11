acf88de038e94da00db76e5e559b95e2e9e2f62f
YOUNGHWA KIM <haileykim2014@gmail.com>
2021-09-11 21:21:26 +0900

    Create 제일작은수제거.java
    1. 배열의 길이가 1이면 -1리턴
    2. 배열의 원소중 최소값 찾기
    -인덱스0번을 최소값으로 지정
    -for문을 돌리면서 최소값 찾기
    3. 최소값 제외한 배열만들기
    -앞서 구한 최소값과 일치하는 원소가있는지 확인
    -나머지 원소들을 배열에넣기

---
+++ Algorithm/src/problemSolving/제일작은수제거.java
@@ -0,0 +1,36 @@
+package problemSolving;
+
+public class 제일작은수제거 {
+	public int[] solution(int[] arr) {
+		if(arr.length==1) {
+			int[] answer = {-1};
+			return answer;
+		}
+		
+		int min = arr[0];
+		for(int i=0;i<arr.length;i++) {
+			if(min>arr[i]) {
+				min=arr[i];
+			}
+		}
+		int[] answer = new int[arr.length-1];
+		int index = 0;
+		for(int i=0;i<arr.length;i++) {
+			if(min==arr[i]) {
+				continue;
+			}
+			answer[index++] = arr[i];
+		}
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		제일작은수제거 t = new 제일작은수제거();
+		int[] arr = {4,3,2,1};
+		arr = t.solution(arr);
+		
+		for(int num:arr)
+		System.out.println(num+" ");
+	}
+
+}
