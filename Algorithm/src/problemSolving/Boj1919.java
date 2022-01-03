2f8f881c5b470f4de359e7f66d03bc2618eae1cf
YOUNGHWA KIM <haileykim2014@gmail.com>
2022-01-03 23:50:37 +0900

    두 수의 합

--- Algorithm/src/problemSolving/Boj1919.java
+++ Algorithm/src/problemSolving/Boj1919.java
@@ -1,33 +1,41 @@
 package problemSolving;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.StringTokenizer;
+import java.util.stream.Stream;
 
-
-public class Boj2577 {
-	public static void main(String args[]) throws IOException {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+public class Boj1919 {
+	public static void main(String[] args) throws IOException{
 		
-		int a = Integer.parseInt(br.readLine());
-		int b = Integer.parseInt(br.readLine());
-		int c = Integer.parseInt(br.readLine());
-		int total = a * b * c;
-		System.out.println("total : "+total);
-		String totalString = String.valueOf(total);
-		int[] arr = new int[100];
-		for(int i = 0 ; i<totalString.length();i++) {
-			arr[totalString.charAt(i)-'0']++;
-			System.out.println("i : "+i +"¹øÂ° :" +totalString.charAt(i));
-		}
-		
-	     for(int i =0;i<10;i++) {
-	    	System.out.println(arr[i]);
-	     }
-
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	int first = Integer.parseInt(br.readLine());
+	int[] arr = new int[first];
+	StringTokenizer st = new StringTokenizer(br.readLine());
+	for(int i=0;i<first;i++) {
+		arr[i] = Integer.parseInt(st.nextToken());
 	}
-
+	int x = Integer.parseInt(br.readLine());
+	Arrays.sort(arr);
+	
+	int start = 0;
+	int end = first -1;
+	int cnt = 0;
+	int sum;
+	while(start<end) {
+		sum= arr[start]+arr[end];
+		if(sum==x)cnt++;
+		if(sum<=x)start++;
+		else end--;
+	}
+	System.out.println(cnt);
+    }
 }
