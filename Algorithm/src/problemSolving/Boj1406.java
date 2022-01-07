5323b236f1f6ac21e81bd48b6c343fb744cb1dc8
YOUNGHWA KIM <haileykim2014@gmail.com>
2022-01-07 22:33:06 +0900

    에디터 링크드 리스트

--- Algorithm/src/problemSolving/Boj1406.java
+++ Algorithm/src/problemSolving/Boj1406.java
@@ -7,35 +7,52 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 import java.util.stream.Stream;
 
-public class Boj1919 {
+public class Boj1406 {
 	public static void main(String[] args) throws IOException{
-		
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	
-	int first = Integer.parseInt(br.readLine());
-	int[] arr = new int[first];
-	StringTokenizer st = new StringTokenizer(br.readLine());
-	for(int i=0;i<first;i++) {
-		arr[i] = Integer.parseInt(st.nextToken());
+		   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	        String[] str = br.readLine().split("");
+	        java.util.LinkedList<Character> list = new java.util.LinkedList<Character>();
+	        ListIterator<Character> iter = list.listIterator();
+
+	        int len = str.length; 
+	        for (int i = 0; i < len; i++) iter.add(str[i].charAt(0));
+
+	        int N = Integer.parseInt(br.readLine());
+
+	        while (N-- > 0) {
+	            char[] input = br.readLine().toCharArray();
+
+	            switch (input[0]) {
+	                case 'L':
+	                    if(iter.hasPrevious())
+	                        iter.previous();
+	                    break;
+	                case 'D':
+	                    if(iter.hasNext())
+	                        iter.next();
+	                    break;
+	                case 'B':
+	                    if(iter.hasPrevious()){
+	                        iter.previous();
+	                        iter.remove();
+	                    }
+	                    break;
+	                default:
+	                    iter.add(input[2]);
+	                    break;
+	            }
+	        }
+	        for(char c : list){
+	            wr.write(c);
+	        }
+	        wr.flush();
 	}
-	int x = Integer.parseInt(br.readLine());
-	Arrays.sort(arr);
-	
-	int start = 0;
-	int end = first -1;
-	int cnt = 0;
-	int sum;
-	while(start<end) {
-		sum= arr[start]+arr[end];
-		if(sum==x)cnt++;
-		if(sum<=x)start++;
-		else end--;
-	}
-	System.out.println(cnt);
-    }
 }
