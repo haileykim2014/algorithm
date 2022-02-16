2023b8cf1a30f5df8567e0fe4f8544780c538a06
YOUNGHWA KIM <haileykim2014@gmail.com>
2022-02-17 00:49:05 +0900

    괄호

--- Algorithm/src/problemSolving/Boj9012.java
+++ Algorithm/src/problemSolving/Boj9012.java
@@ -5,59 +5,43 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-public class Boj2504 {
+public class Boj9012 {
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine();
+
+		int N = Integer.parseInt(br.readLine());
 		Stack<Character> stack = new Stack<>();
-		
-		boolean flag = true;
-		int answer = 0;
-		int temp = 1;
-		
-		for(int i = 0;i<str.length();i++) {
-			char c = str.charAt(i);
-			if(c == '(') {
-				stack.push(c);
-				temp*=2;
-			}
-			else if(c == '[') {
-				stack.push(c);
-				temp*=3;
-			}
-			else {
-				if(c==')') {
-					if(stack.isEmpty()||stack.peek()!='(') {
-						flag= false;
-						break;
-					}
-					if(str.charAt(i-1) == '(') {
-						answer+=temp;
-					}
-					stack.pop();
-					temp/=2;
-				}else if(c==']') {
-					if(stack.isEmpty()||stack.peek()!='[') {
-						flag=false;
-						break;
-					}
-					if(str.charAt(i-1) == '[') {
-						answer += temp;
-					}
-					stack.pop();
-					temp/=3;//[]하나 없어졌으므로 3으로 나누기
+
+		for(int i=0; i<N; i++) {
+			String str = br.readLine();
+			int length = str.length();
+
+			for(int j=0; j<length; j++) {
+				char ch = str.charAt(j);
+				
+				if(ch == '(') {
+					stack.push(ch);
 				}
 				else {
-					flag = false;
-					break;
+					int size = stack.size();
+					if(size == 0) {
+						stack.push(ch);
+						break;
+					}
+					else {
+						stack.pop();
+					}
 				}
 			}
-		}
-		if(!flag||!stack.isEmpty()) {
-			System.out.println(0);
-		}else {
-			System.out.println(answer);
-		}
 
+			if(stack.isEmpty()) {
+				System.out.println("YES");
+			}
+			else {
+				System.out.println("NO");
+			}
+
+			stack.clear();
 	}
+}
 }
