acf678be9e54ddfcd67480df15d8ba00aa5633f2
YOUNGHWA KIM <haileykim2014@gmail.com>
2022-03-16 23:32:40 +0900

    이진수출력 dfs

---
+++ Algorithm/src/problemSolving/이진수출력.java
@@ -0,0 +1,21 @@
+package problemSolving;
+
+import java.util.*;
+class 이진수출력 {
+	public void DFS(int n){
+		if(n==0) return;
+		else{
+			DFS(n/2);
+			System.out.print(n%2);
+		}
+	}
+
+	public void solution(int n){
+		DFS(n);
+	}
+	public static void main(String[] args){
+		이진수출력 T = new 이진수출력();
+		T.solution(11);
+		//System.out.println(T.solution(3));
+	}	
+}
