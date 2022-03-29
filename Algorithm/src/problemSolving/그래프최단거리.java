85cfede23addf2ddf497dc6936980c28b1f2f6ca
YOUNGHWA KIM <haileykim2014@gmail.com>
2022-03-30 00:07:39 +0900

    그래프 최단거리 BFS
    - 주어진 데이터를 담을 변수 선언
    - ch[]방문여부
    - dis[]노드깊이
    - queue 에 순서대로 입력된 연결정보 입력하고 빼기
    - for문을 돌려서 graph에서 매개변수로 받은 인덱스 cv번 arraylist빼낸 연결정보파악
    - 연결정보가 기존에 방문안했다면 방문체크, 방문 정보 넣기
    - 조회한 정보의 깊이는 매개변수로 받은 노드의 자식노드이므로 +1

--- Algorithm/src/problemSolving/그래프최단거리.java
+++ Algorithm/src/problemSolving/그래프최단거리.java
@@ -3,22 +3,22 @@ package problemSolving;
 import java.util.*;
 
 class 그래프최단거리 {
-	static int n, m, answer = 0;
+	static int n, m;
 	static ArrayList<ArrayList<Integer>> graph;
 	static int[] ch, dis;
 
 	public void BFS(int v) {
 		ch[v] = 1;//시작
 		dis[v] = 0;//0단계
-		Queue<Integer> queue = new java.util.LinkedList<>(); //큐 새로 만들기
-		queue.offer(v);
+		Queue<Integer> queue = new java.util.LinkedList<>(); //큐 새로 만들기, 순서를위한것
+		queue.offer(v);//시작점 넣기
 		while (!queue.isEmpty()) { //큐가 빌때까지
 			int cv = queue.poll();//현재 정점꺼내기
-			for (int nv : graph.get(cv)) {
+			for (int nv : graph.get(cv)) {//연결정보 하나씩 꺼내기
 				if (ch[nv] == 0) {//방문안했다면
 					ch[nv] = 1; //방문체크
-					queue.offer(nv);
-					dis[nv] = dis[cv] + 1;
+					queue.offer(nv); //현재 조회한 정보 넣기
+					dis[nv] = dis[cv] + 1; //현재까지 깊이 +1
 				}
 			}
 		}
@@ -36,13 +36,13 @@ class 그래프최단거리 {
 		ch = new int[n + 1]; //인덱스 번호 1번부터하기위해 n+1
 		dis = new int[n + 1];
 		for (int i = 0; i < m; i++) { //인접리스트 만들기
-			int a = scan.nextInt();
-			int b = scan.nextInt();
-			graph.get(a).add(b);
+			int a = scan.nextInt(); //arraylist 순서
+			int b = scan.nextInt(); //넣을 값
+			graph.get(a).add(b); //연결정보 넣기, a번째 arrayList 에 b 넣기
 		}
 		T.BFS(1);
 		for (int i = 2; i <= n; i++) {
-			System.out.println(i + " : " + dis[i]);
+			System.out.println(i + " : " + dis[i]); //노드 : 길이(깊이)
 		}
 	}
 }
