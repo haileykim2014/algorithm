04485a8db3edff39e40e31a81e87e527ac81d362
YOUNGHWA KIM <haileykim2014@gmail.com>
2022-04-01 23:39:50 +0900

    중복순열구하기 DFS

---
+++ Algorithm/src/problemSolving/Permutation.java
@@ -0,0 +1,28 @@
+package problemSolving;
+
+import java.util.*;
+class Permutation {
+	static int[] pm;
+	static int n, m;
+	public void DFS(int L){
+		if(L==m){ //레벨과 인덱스가 같으면
+			for(int x : pm) //배열에 있는거 하나씩 꺼내기
+			System.out.print(x+" ");
+			System.out.println();
+		}
+		else{
+			for(int i=1; i<=n; i++){
+				pm[L]=i;
+				DFS(L+1);
+			}
+		}
+	}
+	public static void main(String[] args){
+		Permutation T = new Permutation();
+		Scanner kb = new Scanner(System.in);
+		n=kb.nextInt(); //원소 갯수
+		m=kb.nextInt(); //인덱스
+		pm=new int[m];
+		T.DFS(0);
+	}
+}
