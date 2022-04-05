26d0eae3bb0269e7ad0ad823eb0ab7d31ea865f2
YOUNGHWA KIM <haileykim2014@gmail.com>
2022-04-06 04:04:10 +0900

    7. 조합수(메모이제이션)

--- Algorithm/src/problemSolving/Memoization.java
+++ Algorithm/src/problemSolving/Memoization.java
@@ -3,10 +3,12 @@ package problemSolving;
 import java.util.*;
 class Memoization{
 	int[][] dy=new int[35][35];
-	public int DFS(int n, int r){
-		if(dy[n][r]>0) return dy[n][r];
+	public int DFS(int n, int r){//n행 r열 ,n개중 r개 뽑는다.
+		if(dy[n][r]>0) //n행r열에 값이 있다면, 
+			return dy[n][r]; //미리 구한 값 리턴
 		if(n==r || r==0) return 1; //제일 하단
-		else return dy[n][r]=DFS(n-1, r-1)+DFS(n-1, r);
+		else return dy[n][r]=DFS(n-1, r-1)+DFS(n-1, r); 
+		//n개중 r개를 뽑는다 = n-1개중 r-1개를 뽑기 + n-1개중 r개뽑기
 	}
 	public static void main(String[] args){
 		Memoization T = new Memoization();
