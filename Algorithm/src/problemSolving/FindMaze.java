e27a48dcad3b66d88d28248006097b308c7ca4c7
hailey <haileykim2014@gmail.com>
2022-04-19 23:31:09 +0900

    미로탐색 DFS

--- Algorithm/src/problemSolving/FindMaze.java
+++ Algorithm/src/problemSolving/FindMaze.java
@@ -2,21 +2,21 @@ package problemSolving;
 
 import java.util.*;
 class FindMaze {
-	static int[] dx={-1, 0, 1, 0};
+	static int[] dx={-1, 0, 1, 0}; //동서남북
 	static int[] dy={0, 1, 0, -1};
 	static int[][] board;
 	static int answer=0;
 
 	public void DFS(int x, int y){
-		if(x==7 && y==7) answer++;
+		if(x==7 && y==7) answer++; //종착지도착
 		else{
 			for(int i=0; i<4; i++){
 				int nx=x+dx[i];
 				int ny=y+dy[i];
-				if(nx>=1 && nx<=7 && ny>=1 && ny<=7 && board[nx][ny]==0){
-					board[nx][ny]=1;
+				if(nx>=1 && nx<=7 && ny>=1 && ny<=7 && board[nx][ny]==0){ //nx<=7경계선안쪽
+					board[nx][ny]=1; //방문확인
 					DFS(nx, ny);
-					board[nx][ny]=0;
+					board[nx][ny]=0; //back
 				}
 			}
 		}	
